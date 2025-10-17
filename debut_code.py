26c393a14fc6409e2deb010787be62f23bd0a137
FinOrm-dev <romeohuynh@hotmail.com>
2025-10-17 10:05:58 +0200

    Debut du travail de Roméo

--- debut_code.py
+++ debut_code.py
@@ -3,15 +3,58 @@ from turtle import*
 
 setup(1000,800)
 speed(0)
+title("Projet de Roméo!!")
+
+def The_Cat():
+    up()
+    goto(0,0)
+    down()
+    global Champ_taille
+    x=100
+    begin_fill()
+    setheading(180)
+    circle(x,90)
+    setheading(0)
+    fd(x*2)
+    circle(x,90)
+    end_fill()
+    begin_fill()
+    circle(x,90)
+    fd(x*2)
+    setheading(270)
+    circle(x,90)
+    fd(x*2)
+    end_fill()
+    backward((x*3)-(1.3*x))
+    setheading(90)
+    begin_fill()
+    fd(x*2.3)
+    setheading(180)
+    circle((1/3)*(x*1.3),90)
+    setheading(180)
+    fd((1/3)*(x*1.3))
+    setheading(90)
+    circle((1/3)*(x*1.3),90)
+    setheading(270)
+    fd(x*1.3)    
+    end_fill()
+    hideturtle()
 
 
-def figure1():
-    begin_fill(color)
-    fd(50)
+def The_peroquet():
+    up()
+    goto(0,0)
+    down()
+    global Champ_taille
+    x=100   
+    begin_fill()
+
 
 def figure2():
     backward(50)
+button("Chat", The_Cat)
+button("Peroquet", The_peroquet)
+Champ_taille=entry("La taille de la figure: ")
 
-button("figure1", figure1)
-slider("figure2", 0,100)
+
 mainloop()
