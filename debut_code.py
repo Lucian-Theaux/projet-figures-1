e879c9c8060ce62bff4805751ed2d5cbe1c7a1e9
Romeo Huynh <romeohuynh@hotmail.com>
2025-10-20 11:33:53 +0200

    Figures finales sans+
    Added background color to the turtle graphics window.

--- debut_code.py
+++ debut_code.py
@@ -4,9 +4,10 @@ from turtle import*             #On importe le module turtle qui permet de dessi
 setup(1000,800)                 #Ici on set up le fromat de l'écran
 speed(0)                        #On définie la vitesse de la tortue à 0 (=elle ne fait pas d'animation, elle trace directement)
 title("Projet de Roméo!!")      #On met un titre à la fenêtre
+bgcolor("lightblue")          #On choisit une couleur de fond pour la fenêtre
 hideturtle()                    #On cache la tortue pour plus l'esthétique (personnellement)
 
-x=100                           #Variable temporaire et global pour la taille des figures
+x=100                          #Variable temporaire et global pour la taille des figures
 
 def The_Cat():                  #On commence la définition de la fonction qui dessine "The_Cat"             
     global x                    #On précise que x est une variable globale
@@ -82,51 +83,51 @@ def The_perroquet():             #On commence la définition de la fonction qui 
 def The_Snake():                #On commence la définition de la fonction qui dessine "The_Snake"
     global x
     x3=x*(3.35/4)               #Création d'une troisième variable x3 pour que le serpent ait un taille proportionelle à la hauteur du chat
-    x4=0                        #Variable pour le déplacement horizontal du serpent
-    y1=180                      #Variables pour l'orientation des cercles (180° soit l'orientation vers la gauche)
-    y2=0                        #On initialise y2 qui représente 0° soit l'orientation vers la droite
-    r2=x*(3.35/4)               #Rayon des cercles du serpent, variable ^pour l'instant égale à x3 mais qui va representer l'opposé de x3 lors du deuxieme passage dans la boucle
-    r3=x                        #Rayon des yeux du serpent, variable pour l'instant égale à x mais qui va representer l'opposé de x lors du deuxieme passage dans la boucle
+    xy=0                        #Variable pour le déplacement horizontal du serpent
+    orientation_cercle1=180     #Variables pour l'orientation des cercles (180° soit l'orientation vers la gauche)
+    orientation_cercle2=0                        #On initialise y2 qui représente 0° soit l'orientation vers la droite
+    sens_cercle1=x*(3.35/4)               #Rayon des cercles du serpent, variable ^pour l'instant égale à x3 mais qui va representer l'opposé de x3 lors du deuxieme passage dans la boucle
+    sens_cercle2=x                        #Rayon des yeux du serpent, variable pour l'instant égale à x mais qui va representer l'opposé de x lors du deuxieme passage dans la boucle
     for i in range(2):          #On fait une boucle pour dessiner les deux serpents (gauche et droite)
         up()
-        goto(x4,-((x3)*4.5))
-        x4=x4+2*x3              #On change la position horizontale pour le serpent de droite pour qu'il soit collé à droite du serpent de gauche
+        goto(xy,-((x3)*4.5))
+        xy=xy+2*x3              #On change la position horizontale pour le serpent de droite pour qu'il soit collé à droite du serpent de gauche
         down()
         fillcolor("black")
         """On commence à desssiner Le serpent de gauche"""
         begin_fill()
         setheading(90)
         fd(x3)                  #On avance de x3 pour se placer au bon endroit pour dessiner le premier quart de cercle
-        setheading(y1)          #On oriente la tortue vers la gauche ou la droite en fonction de la valeur de y1
-        circle(r2,90)           #On dessine un quart de cercle de rayon r2
-        setheading(y2)          #On oriente la tortue vers la droite ou la gauche en fonction de la valeur de y2
+        setheading(orientation_cercle1)          #On oriente la tortue vers la gauche ou la droite en fonction de la valeur de y1
+        circle(sens_cercle1,90)           #On dessine un quart de cercle de rayon r2
+        setheading(orientation_cercle2)          #On oriente la tortue vers la droite ou la gauche en fonction de la valeur de y2
         fd(x3)
-        circle(r2,180)
+        circle(sens_cercle1,180)
         setheading(90)
         fd(x3)
-        setheading(y1)
-        circle(r2,180)
+        setheading(orientation_cercle1)
+        circle(sens_cercle1,180)
         setheading(90)
         fd(x3)
-        setheading(y2)
-        circle(r2,180)
+        setheading(orientation_cercle2)
+        circle(sens_cercle1,180)
         fd(x3)
         setheading(270)
-        circle(r2,90)
+        circle(sens_cercle1,90)
         end_fill()
         setheading(90)
         fd((x)*(6/10))          #On avance pour se placer au bon endroit pour dessiner les yeux
         fillcolor("white")
         """Dans cette partie on dessine les yeux du serpent"""
         begin_fill()
-        circle(r3*(1/10))       #On dessine le cercle blanc de l'œil avec un rayon proportionelle au rayon de la tête du serpent
+        circle(sens_cercle2*(1/10))       #On dessine le cercle blanc de l'œil avec un rayon proportionelle au rayon de la tête du serpent
         end_fill()
         """!Partie très importante de la fonction The_Snake! Ici on inverse les variables pour dessiner le serpent de droite de façon symétrique au serpent de gauche"""
-        ybis=y1                 #On utilise une variable temporaire ybis pour échanger les valeurs de y1 et y2 (elle prend la valeur de y1)
-        y1=y2                   #Y1 prend la valeur de y2
-        y2=ybis                 #Y2 prend la valeur de ybis (donc la valeur initiale de y1)
-        r2=-r2                  #On inverse le signe de r2 pour que les cercles soient dessinés dans l'autre sens
-        r3=-r3                  #On inverse le signe de r3 pour que les yeux(=cercle) soient dessinés dans l'autre sens
+        variable_tampon=orientation_cercle1                 #On utilise une variable temporaire variable_tampon pour échanger les valeurs de orientation_cercle1 et orientation_cercle2 (elle prend la valeur de orientation_cercle1)
+        orientation_cercle1=orientation_cercle2             #orientation_cercle1 prend la valeur de orientation_cercle2
+        orientation_cercle2=variable_tampon                 #orientation_cercle2 prend la valeur de varaible_tampon(donc la valeur initiale de orientation_cercle1)
+        sens_cercle1=-sens_cercle1                  #On inverse le signe de se,s_cercle1 pour que les cercles soient dessinés dans l'autre sens(antihoraire/horraire)
+        sens_cercle2=-sens_cercle2                  #On inverse le signe de sens_cercle2 pour que les yeux(=cercle) soient dessinés dans l'autre sens(antihoraire/horraire)
 
 
 
@@ -139,4 +140,3 @@ button("Serpent", The_Snake)
 
 
 mainloop()                                          #Fonction pour garder la fenêtre graphique ouverte 
-
