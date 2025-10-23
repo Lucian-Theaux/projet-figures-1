b05d99bb4437af3fa184569117c27a02c8252c00
Romeo Huynh <romeohuynh@hotmail.com>
2025-10-23 16:21:51 +0200

    Update main.py

--- projet final/main.py
+++ projet final/main.py
@@ -36,7 +36,7 @@ def The_Cat():                  #On commence la définition de la fonction qui d
     right(90)
     fd((1/3)*(metric*1.5))           #On avance de la largeur entre les oreilles (1/3 de la largeur de la tête)
     right(90)
-    circle((1/3)*(metric*1.5),90)    #Deumetricième oreille
+    circle((1/3)*(metric*1.5),90)    #Deuxième oreille
     left(90)
     fd(metric*1.3)    
     end_fill()                  #On termine le remplissage de la tête
@@ -64,7 +64,7 @@ def The_perroquet():             #On commence la définition de la fonction qui 
     setheading(90)
     fd((metric2)*2)
     setheading(180)
-    fillcolor("white")          #On change la couleur de remplissage en blanc pour les yeumetric
+    fillcolor("white")          #On change la couleur de remplissage en blanc pour les yeux
     begin_fill()
     circle((metric2)/2,180)          #On dessine un demi cercle pour l'œil gauche 2fois plus petit que la taille du cercle du corps noir du perroquet
     end_fill()
@@ -80,8 +80,8 @@ def The_Snake():                #On commence la définition de la fonction qui d
     metric3=metric*(3.35/4)               #Création d'une troisième variable metric3 pour que le serpent ait un taille proportionelle à la hauteur du chat
     metricy=0                        #Variable pour le déplacement horizontal du serpent
     orientation_cercle1=180     #Variables pour l'orientation des cercles (180° soit l'orientation vers la gauche)
-    orientation_cercle2=0                        #On initialise y2 qui représente 0° soit l'orientation vers la droite
-    sens_cercle1=metric*(3.35/4)               #Rayon des cercles du serpent, variable ^pour l'instant égale à metric3 mais qui va representer l'opposé de metric3 lors du deumetricieme passage dans la boucle
+    orientation_cercle2=0                        #On initialise orentation_cercle2 qui représente 0° soit l'orientation vers la droite
+    sens_cercle1=metric*(3.35/4)               #Rayon des cercles du serpent, variable pour l'instant égale à metric3 mais qui va representer l'opposé de metric3 lors du deumetricieme passage dans la boucle
     sens_cercle2=metric                        #Rayon des yeumetric du serpent, variable pour l'instant égale à metric mais qui va representer l'opposé de metric lors du deumetricieme passage dans la boucle
     for i in range(2):          #On fait une boucle pour dessiner les deumetric serpents (gauche et droite)
         up()
@@ -93,9 +93,9 @@ def The_Snake():                #On commence la définition de la fonction qui d
         begin_fill()
         setheading(90)
         fd(metric3)                  #On avance de metric3 pour se placer au bon endroit pour dessiner le premier quart de cercle
-        setheading(orientation_cercle1)          #On oriente la tortue vers la gauche ou la droite en fonction de la valeur de y1
-        circle(sens_cercle1,90)           #On dessine un quart de cercle de rayon r2
-        setheading(orientation_cercle2)          #On oriente la tortue vers la droite ou la gauche en fonction de la valeur de y2
+        setheading(orientation_cercle1)          #On oriente la tortue vers la gauche ou la droite en fonction de la valeur de orientation_cercle1
+        circle(sens_cercle1,90)           #On dessine un quart de cercle de rayon sens_cercle1
+        setheading(orientation_cercle2)          #On oriente la tortue vers la droite ou la gauche en fonction de la valeur de orientation_cercle2
         fd(metric3)
         circle(sens_cercle1,180)
         setheading(90)
@@ -111,9 +111,9 @@ def The_Snake():                #On commence la définition de la fonction qui d
         circle(sens_cercle1,90)
         end_fill()
         setheading(90)
-        fd((metric)*(6/10))          #On avance pour se placer au bon endroit pour dessiner les yeumetric
+        fd((metric)*(6/10))          #On avance pour se placer au bon endroit pour dessiner les yeux
         fillcolor("white")
-        """Dans cette partie on dessine les yeumetric du serpent"""
+        """Dans cette partie on dessine les yeux du serpent"""
         begin_fill()
         circle(sens_cercle2*(1/10))       #On dessine le cercle blanc de l'œil avec un rayon proportionelle au rayon de la tête du serpent
         end_fill()
@@ -121,8 +121,8 @@ def The_Snake():                #On commence la définition de la fonction qui d
         variable_tampon=orientation_cercle1                 #On utilise une variable temporaire variable_tampon pour échanger les valeurs de orientation_cercle1 et orientation_cercle2 (elle prend la valeur de orientation_cercle1)
         orientation_cercle1=orientation_cercle2             #orientation_cercle1 prend la valeur de orientation_cercle2
         orientation_cercle2=variable_tampon                 #orientation_cercle2 prend la valeur de varaible_tampon(donc la valeur initiale de orientation_cercle1)
-        sens_cercle1=-sens_cercle1                  #On inverse le signe de se,s_cercle1 pour que les cercles soient dessinés dans l'autre sens(antihoraire/horraire)
-        sens_cercle2=-sens_cercle2                  #On inverse le signe de sens_cercle2 pour que les yeumetric(=cercle) soient dessinés dans l'autre sens(antihoraire/horraire)
+        sens_cercle1=-sens_cercle1                  #On inverse le signe de sens_cercle1 pour que les cercles soient dessinés dans l'autre sens(antihoraire/horraire)
+        sens_cercle2=-sens_cercle2                  #On inverse le signe de sens_cercle2 pour que les yeux(=cercle) soient dessinés dans l'autre sens(antihoraire/horraire)
 
 
 #* ------------------- PROGRAMMES DE NATHAN EUDELIN ------------------- *#
